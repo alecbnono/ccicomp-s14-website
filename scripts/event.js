a0001074aa5a2f7ff8726211efdbfd341e919705
jiancpp <117244684+jiancpp@users.noreply.github.com>
2025-04-06 10:40:45 +0800

    Added Event Highlights

---
+++ scripts/event.js
@@ -0,0 +1,16 @@
+new Swiper('.card-wrapper', {
+    loop: true,
+  
+    // Pagination bullets
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }  
+  });
